d9b04a85781dd3c947bdbe0d55c035f8f9939188
Veronica Valenzuela <vmvalenzuela24@gmail.com>
2024-05-08 11:44:10 -0700

    running prettier and eslint on user and ui

--- packages/user/src/ScopedRecord.ts
+++ packages/user/src/ScopedRecord.ts
@@ -1,4 +1,13 @@
-import { Columns, StringColumn, Record, withRecordColumns, getDb, Table, getColumnByName, getTables } from '@proteinjs/db';
+import {
+  Columns,
+  StringColumn,
+  Record,
+  withRecordColumns,
+  getDb,
+  Table,
+  getColumnByName,
+  getTables,
+} from '@proteinjs/db';
 import { UserRepo } from './UserRepo';
 
 export interface ScopedRecord extends Record {
@@ -8,21 +17,21 @@ export interface ScopedRecord extends Record {
 export const getScopedDb = getDb<ScopedRecord>;
 
 const scopedRecordColumns = {
-  scope: new StringColumn('scope', { 
-    defaultValue: async () =>  new UserRepo().getUser().id,
-    addToQuery: async (qb) => { 
-      qb.condition({ 
-        field: 'scope', 
-        operator: 'IN', 
-        value: [new UserRepo().getUser().id]
+  scope: new StringColumn('scope', {
+    defaultValue: async () => new UserRepo().getUser().id,
+    addToQuery: async (qb) => {
+      qb.condition({
+        field: 'scope',
+        operator: 'IN',
+        value: [new UserRepo().getUser().id],
       });
     },
     ui: { hidden: true },
   }),
-}
+};
 
 export function getScopedTables() {
-  return getTables<ScopedRecord>().filter(table => isScopedTable(table));
+  return getTables<ScopedRecord>().filter((table) => isScopedTable(table));
 }
 
 export function isScopedTable(table: Table<any>) {
@@ -32,12 +41,14 @@ export function isScopedTable(table: Table<any>) {
 
 /**
  * Wrapper function to add default ScopedRecord columns to your table's columns.
- * 
+ *
  * Note: using this requires an explicit dependency on moment@2.29.4 in your package (since transient dependencies are brittle by typescript's standards)
- * 
+ *
  * @param columns your columns
  * @returns recordColumns & sourceRecordColumns & your columns
  */
-export function withScopedRecordColumns<T extends ScopedRecord>(columns: Columns<Omit<T, keyof ScopedRecord>>): Columns<ScopedRecord> & Columns<Omit<T, keyof ScopedRecord>> {
+export function withScopedRecordColumns<T extends ScopedRecord>(
+  columns: Columns<Omit<T, keyof ScopedRecord>>
+): Columns<ScopedRecord> & Columns<Omit<T, keyof ScopedRecord>> {
   return Object.assign(Object.assign({}, scopedRecordColumns), withRecordColumns<Record>(columns) as any);
 }
